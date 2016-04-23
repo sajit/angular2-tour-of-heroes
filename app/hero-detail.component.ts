25fe03ed5da3701138de2563d682b5a013d00303
sajit <sajit.kunnumkal@gmail.com>
2016-04-23 12:04:59 -0400

    Move all css files out

--- app/hero-detail.component.ts
+++ app/hero-detail.component.ts
@@ -6,8 +6,8 @@ import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-hero-detail',
-  templateUrl: 'app/hero-detail.component.html',
-  styleUrls: ['app/hero-detail.component.css']
+  templateUrl: 'app/views/hero-detail.component.html',
+  styleUrls: ['app/styles/hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
